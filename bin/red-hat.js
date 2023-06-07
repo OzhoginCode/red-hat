acc21a85b38972f9ee34384467abd30224010e64
Dmitrii Smirnov <dismirow@gmail.com>
2023-06-08 01:31:53 +0300

    test pull requests

---
+++ bin/red-hat.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startGame from '../src/games/red-hat.js';
+
+startGame();
---
+++ src/menu.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const startMenu = () =>{
+    const name = readlineSync.question(
+        'Добро пожаловать в игру "Сказочный замес"! Как тебя зовут, герой? ')
+    const menuItems = ['Новая игра', 'Таблица рекордов',];
+    const index = readlineSync.keyInSelect(menuItems, 'Выберите пункт меню:',
+        {cancel: 'Выйти из игры'});
+
+    console.log(`${name}, вы выбрали ${menuItems[index]}!`);
+}
+startMenu()
