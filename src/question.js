20457a305d098b02f5a962c3ffddeb250bc9fe86
Sergunkit <sergunkit@mail.ru>
2023-06-13 23:59:29 +0300

    change question

--- src/question.js
+++ src/question.js
@@ -2,24 +2,25 @@ import readlineSync from 'readline-sync';
 import getDataFromFile from './fs.js';
 import * as game from './red-hat.js';
 
-const avatars = { speaker: 'hiya', mother: 'yasuna_02', ombudsman: 'C3PO' };
 export default (currentHero, taskNum) => {
+  const avatars = { speaker: 'hiya', mother: 'yasuna_02', ombudsman: 'C3PO' };
   const tasks = getDataFromFile('./content/riddles.json');
   game.sayPhrase(tasks[taskNum - 1].riddle, avatars.ombudsman);
   const answer = readlineSync.keyInSelect(
     tasks[taskNum - 1].answers,
     'Выберите правильный ответ:',
     { cancel: 'Выйти из игры' },
-  );
+  ) + 1;
   const { score } = currentHero;
   const { complexity } = tasks[taskNum - 1];
+  let hero = { ...currentHero, score: score + complexity, win: true };
   if (tasks[taskNum - 1].correctAnswer === answer) {
-    return { ...currentHero, win: true, score: score + complexity };
+    console.log('ПРАВИЛЬНЫЙ ОТВЕТ!');
+    return hero;
   }
+  console.log('Не верный ответ.');
   const lostPies = complexity >= 5 ? 2 : 1;
   const { count } = currentHero;
-  return { ...currentHero, win: false, count: count - lostPies };
+  hero = { ...currentHero, count: count - lostPies, win: false };
+  return hero;
 };
-
-const tasks = getDataFromFile('./content/riddles.json');
-console.log(tasks[0].riddle);
